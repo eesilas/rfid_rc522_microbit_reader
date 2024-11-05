a0800e8e4804c19cb4bef54ca9fa8f2fc0000656
Learn by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-05 17:19:44 +0800

    Read RFID's ID to display on micro:bit.

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
-MFRC522.Init()
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     serial.writeLine("ID:" + MFRC522.getID())
     MFRC522.write("12345")
     serial.writeLine("Data:" + MFRC522.read())
+})
+let myData = ""
+let myID = ""
+MFRC522.Init()
+basic.showIcon(IconNames.SmallHeart)
+basic.forever(function () {
+    myID = "ID:" + MFRC522.getID()
+    basic.showString(myID)
+    myData = MFRC522.read()
+    basic.showString(myData)
 })
