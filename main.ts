23015a01fd049795a9ecec0b86fc8dd07d4dc5a0
Learn by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-06 09:47:51 +0800

    Initial release of RFID reader dev.

--- main.ts
+++ main.ts
@@ -3,13 +3,59 @@ input.onButtonPressed(Button.A, function () {
     MFRC522.write("12345")
     serial.writeLine("Data:" + MFRC522.read())
 })
-let myData = ""
-let myID = ""
-MFRC522.Init()
-basic.showIcon(IconNames.SmallHeart)
-basic.forever(function () {
+function init () {
+    MFRC522.Init()
+    reading()
+    basic.clearScreen()
+    text_list = ["730974132981", "108292839542", "591664682793"]
+}
+input.onButtonPressed(Button.B, function () {
     myID = "ID:" + MFRC522.getID()
     basic.showString(myID)
     myData = MFRC522.read()
     basic.showString(myData)
+})
+function reading () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # #
+        . . . . .
+        . . . . .
+        `)
+}
+let myData = ""
+let myID = ""
+let text_list: string[] = []
+init()
+basic.forever(function () {
+    if (MFRC522.getID() == 730974132981) {
+        basic.showIcon(IconNames.Giraffe)
+        basic.clearScreen()
+    } else if (MFRC522.getID() == 108292839542) {
+        basic.showIcon(IconNames.Snake)
+        basic.clearScreen()
+    } else if (MFRC522.getID() == 591664682793) {
+        basic.showIcon(IconNames.Duck)
+        basic.clearScreen()
+    } else {
+        reading()
+        basic.clearScreen()
+    }
 })
