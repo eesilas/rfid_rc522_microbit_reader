5a7fd0a2d14751ecaab6dac9fcc00b8abff14fba
Learn by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-06 12:04:55 +0800

    ba ding sound effect add at pin 0 to amplifier.

--- main.ts
+++ main.ts
@@ -2,14 +2,6 @@ input.onButtonPressed(Button.A, function () {
     serial.writeLine("ID:" + MFRC522.getID())
     MFRC522.write("12345")
     serial.writeLine("Data:" + MFRC522.read())
-})
-function init () {
-    MFRC522.Init()
-    reading()
-    basic.clearScreen()
-    text_list = ["730974132981", "108292839542", "591664682793"]
-}
-input.onButtonPressed(Button.B, function () {
     myID = "ID:" + MFRC522.getID()
     basic.showString(myID)
     myData = MFRC522.read()
@@ -42,19 +34,28 @@ function reading () {
 }
 let myData = ""
 let myID = ""
-let text_list: string[] = []
+MFRC522.Init()
+reading()
+basic.clearScreen()
+radio.setTransmitPower(7)
+radio.setGroup(123)
 pins.setAudioPin(DigitalPin.P0)
 music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
-init()
 basic.forever(function () {
     if (MFRC522.getID() == 730974132981) {
+        radio.sendNumber(111)
         basic.showIcon(IconNames.Giraffe)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpDown), music.PlaybackMode.InBackground)
         basic.clearScreen()
     } else if (MFRC522.getID() == 108292839542) {
+        radio.sendNumber(112)
         basic.showIcon(IconNames.Snake)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpDown), music.PlaybackMode.InBackground)
         basic.clearScreen()
     } else if (MFRC522.getID() == 591664682793) {
+        radio.sendNumber(113)
         basic.showIcon(IconNames.Duck)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpDown), music.PlaybackMode.InBackground)
         basic.clearScreen()
     } else {
         reading()
